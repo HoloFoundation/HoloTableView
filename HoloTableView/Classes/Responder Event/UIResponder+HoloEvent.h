a7e59959854ace1976789def757bf9a7f891bf7d
gonghonglou <gonghonglou@icloud.com>
2019-12-05 22:40:15 +0800

    params can be nil

--- HoloTableView/Classes/Responder Event/UIResponder+HoloEvent.h
+++ HoloTableView/Classes/Responder Event/UIResponder+HoloEvent.h
@@ -7,12 +7,8 @@
 
 #import <UIKit/UIKit.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface UIResponder (HoloEvent)
 
 - (void)holo_event:(NSString *)event params:(NSDictionary *)params;
 
 @end
-
-NS_ASSUME_NONNULL_END
