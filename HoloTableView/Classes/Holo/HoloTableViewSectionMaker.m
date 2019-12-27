2c26d5edf7d06f3eb7255ff634b73eb719eef4f1
gonghonglou <gonghonglou@icloud.com>
2019-12-27 23:27:39 +0800

    simplify the code

--- HoloTableView/Classes/Holo/HoloTableViewSectionMaker.m
+++ HoloTableView/Classes/Holo/HoloTableViewSectionMaker.m
@@ -211,12 +211,11 @@
         _isRemark = isRemark;
         
         for (HoloTableSection *section in self.targetSections) {
-            
             NSString *dictKey = section.tag ?: kHoloSectionTagNil;
             if (self.sectionIndexsDict[dictKey]) continue;
             
             NSMutableDictionary *dict = @{kHoloTargetSection : section}.mutableCopy;
-            dict[kHoloTargetIndex] = [NSNumber numberWithInteger:[self.targetSections indexOfObject:section]];
+            dict[kHoloTargetIndex] = @([self.targetSections indexOfObject:section]);
             self.sectionIndexsDict[dictKey] = [dict copy];
         }
     }
