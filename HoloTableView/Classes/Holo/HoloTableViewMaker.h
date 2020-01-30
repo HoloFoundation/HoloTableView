bec2098d5f9d76fe941e1c0d656dfef5d702439f
gonghonglou <gonghonglou@icloud.com>
2020-01-30 23:53:49 +0800

    rename

---
+++ HoloTableView/Classes/Holo/HoloTableViewMaker.h
@@ -0,0 +1,16 @@
+//
+//  HoloTableViewMaker.h
+//  HoloTableView
+//
+//  Created by 与佳期 on 2020/1/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HoloTableViewMaker : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HoloTableView/Classes/Holo/HoloTableViewMaker.m
@@ -0,0 +1,12 @@
+//
+//  HoloTableViewMaker.m
+//  HoloTableView
+//
+//  Created by 与佳期 on 2020/1/30.
+//
+
+#import "HoloTableViewMaker.h"
+
+@implementation HoloTableViewMaker
+
+@end
