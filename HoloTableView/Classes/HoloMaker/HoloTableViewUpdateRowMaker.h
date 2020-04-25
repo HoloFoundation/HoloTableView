249fbb2dae33cc2592b64c62035a89713eeaa495
gonghonglou <gonghonglou@icloud.com>
2020-04-25 20:59:07 +0800

    update import

--- HoloTableView/Classes/HoloMaker/HoloTableViewUpdateRowMaker.h
+++ HoloTableView/Classes/HoloMaker/HoloTableViewUpdateRowMaker.h
@@ -6,8 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "HoloTableViewRowMaker.h"
-@class HoloTableSection;
+@class HoloTableSection, HoloTableRowMaker, HoloTableRow;
 
 NS_ASSUME_NONNULL_BEGIN
 
