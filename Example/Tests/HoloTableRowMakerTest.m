308207dc18f3565adcfaf3eb781d00198ad9236a
gonghonglou <gonghonglou@icloud.com>
2021-05-20 18:50:40 +0800

    unit test case for: holo_removeRows: method

--- Example/Tests/HoloTableRowMakerTest.m
+++ Example/Tests/HoloTableRowMakerTest.m
@@ -254,6 +254,26 @@
     XCTAssertEqual(row4.height, 4);
 }
 
+- (void)testRemoveRows {
+    [self.tableView holo_makeRows:^(HoloTableViewRowMaker * _Nonnull make) {
+        make.row(UITableViewCell.class).tag(@"1");
+        make.row(UITableViewCell.class).tag(@"2");
+        make.row(UITableViewCell.class).tag(@"2");
+        make.row(UITableViewCell.class).tag(@"3");
+    }];
+    XCTAssertEqual(self.tableView.holo_sections.firstObject.rows.count, 5);
+    
+    [self.tableView holo_removeRows:@[@"1", @"2"]];
+    
+    XCTAssertEqual(self.tableView.holo_sections.firstObject.rows.count, 2);
+    
+    HoloTableRow *lastRow = self.tableView.holo_sections.firstObject.rows.lastObject;
+    XCTAssertEqual(lastRow.tag, @"3");
+
+    [self.tableView holo_removeAllSections];
+    XCTAssertEqual(self.tableView.holo_sections.firstObject.rows.count, 0);
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
