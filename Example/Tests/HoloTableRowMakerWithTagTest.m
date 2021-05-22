594581b32c32ca429aa943c5bcf20274111b20cf
gonghonglou <gonghonglou@icloud.com>
2021-05-22 17:27:59 +0800

    unit test case for: `holo_updateRowsInSection:`, `holo_remakeRowsInSection:` and `holo_insertRowsAtIndex:inSection:`

--- Example/Tests/HoloTableRowMakerWithTagTest.m
+++ Example/Tests/HoloTableRowMakerWithTagTest.m
@@ -64,7 +64,75 @@
 }
 
 - (void)testUpdateRowsInSection {
+    [self.tableView holo_updateRowsInSection:@"1" block:^(HoloTableViewUpdateRowMaker * _Nonnull make) {
+        make.tag(@"0").height(1000);
+    }];
     
+    XCTAssertEqual(self.tableView.holo_sections.count, 3);
+    
+    HoloTableSection *section0 = self.tableView.holo_sections[0];
+    HoloTableSection *section1 = self.tableView.holo_sections[1];
+    HoloTableSection *section2 = self.tableView.holo_sections[2];
+    
+    XCTAssertEqual(section0.rows.count, 2);
+    XCTAssertEqual(section1.rows.count, 2);
+    XCTAssertEqual(section2.rows.count, 2);
+    
+    HoloTableRow *row0InSection1 = section1.rows[0];
+    HoloTableRow *row1InSection1 = section1.rows[1];
+    
+    XCTAssertEqual(row0InSection1.height, 1000);
+    XCTAssertEqual(row1InSection1.height, 1);
+    
+    HoloTableRow *row0InSection2 = section2.rows[0];
+    HoloTableRow *row1InSection2 = section2.rows[1];
+    
+    XCTAssertEqual(row0InSection2.height, 0);
+    XCTAssertEqual(row1InSection2.height, 1);
+}
+
+- (void)testRemakeRowsInSection {
+    [self.tableView holo_remakeRowsInSection:@"1" block:^(HoloTableViewUpdateRowMaker * _Nonnull make) {
+        make.tag(@"0").row(UITableViewCell.class);
+    }];
+    
+    XCTAssertEqual(self.tableView.holo_sections.count, 3);
+    
+    HoloTableSection *section0 = self.tableView.holo_sections[0];
+    HoloTableSection *section1 = self.tableView.holo_sections[1];
+    HoloTableSection *section2 = self.tableView.holo_sections[2];
+    
+    XCTAssertEqual(section0.rows.count, 2);
+    XCTAssertEqual(section1.rows.count, 2);
+    XCTAssertEqual(section2.rows.count, 2);
+    
+    HoloTableRow *row0InSection1 = section1.rows[0];
+    HoloTableRow *row1InSection1 = section1.rows[1];
+    
+    XCTAssertEqual(row0InSection1.height, CGFLOAT_MIN);
+    XCTAssertEqual(row1InSection1.height, 1);
+    
+    HoloTableRow *row0InSection2 = section2.rows[0];
+    HoloTableRow *row1InSection2 = section2.rows[1];
+    
+    XCTAssertEqual(row0InSection2.height, 0);
+    XCTAssertEqual(row1InSection2.height, 1);
+}
+
+- (void)testInsertRowsInSection {
+    [self.tableView holo_insertRowsAtIndex:2 inSection:@"1" block:^(HoloTableViewRowMaker * _Nonnull make) {
+        make.row(UITableViewCell.class).tag(@"2").model(@"2").height(2);
+    }];
+    
+    XCTAssertEqual(self.tableView.holo_sections.count, 3);
+    
+    HoloTableSection *section0 = self.tableView.holo_sections[0];
+    HoloTableSection *section1 = self.tableView.holo_sections[1];
+    HoloTableSection *section2 = self.tableView.holo_sections[2];
+    
+    XCTAssertEqual(section0.rows.count, 2);
+    XCTAssertEqual(section1.rows.count, 3);
+    XCTAssertEqual(section2.rows.count, 2);
 }
 
 - (void)testPerformanceExample {
